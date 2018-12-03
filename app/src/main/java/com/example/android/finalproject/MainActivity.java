6a1aa1d6a30fc1dc040a2052cbfe886881cb3a7a
Alex Li <ali50@illinois.edu>
2018-12-03 16:17:23 -0600

    idk what i'm doing

--- app/src/main/java/com/example/android/finalproject/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject/MainActivity.java
@@ -8,28 +8,19 @@ import android.widget.Button;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.onClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
     }
-    Button button = (Button) findViewById(R.id.button);
-
-    button.setOnClickListener(new View.onClickListener() {
-
-        @Override
-
-        public void onClick(View v) {
-
-            goToSecondActivity();
-
-        }
-
-    });
     private void goToSecondActivity() {
 
         Intent intent = new Intent(this, drinks_and_sizes.class);
