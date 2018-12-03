7bd28b7dd52801e71a0c2683e211ec1b9abc6d81
Alex Li <ali50@illinois.edu>
2018-12-03 15:45:50 -0600

    idk what i'm doing

--- app/src/main/java/com/example/android/finalproject/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.finalproject;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.onClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                goToSecondActivity();
+
+            }
+
+        });
+    }
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        startActivity(intent);
+
     }
 }
