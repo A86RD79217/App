ff140caec88852f0d3f928435734aa6115406de8
arjun <arjunckatz@gmail.com>
2018-12-03 17:25:51 -0600

    main acivities

---
+++ app/src/main/java/com/example/android/finalproject/College_at_party.java
@@ -0,0 +1,13 @@
+package com.example.android.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class College_at_party extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_at_party);
+    }
+}
