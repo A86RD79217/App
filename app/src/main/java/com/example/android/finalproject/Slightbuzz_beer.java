9d9617cdaf8876765ba9bafd4fd3da349d907332
A86RD79217 <alex354M@gmail.com>
2018-12-04 16:19:06 -0600

    slightly buzz beer done

--- app/src/main/java/com/example/android/finalproject/Slightbuzz_beer.java
+++ app/src/main/java/com/example/android/finalproject/Slightbuzz_beer.java
@@ -1,7 +1,15 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
 
 public class Slightbuzz_beer extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class Slightbuzz_beer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slightbuzz_beer);
+        Button home = (Button) findViewById(R.id.SBbeerhome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+                goToInsertNameOfNextClass();
+            }
+        });
+    }
+    private void goToInsertNameOfNextClass() {
+        Intent intent = new Intent(this, Main_dashboard.class);
+        startActivity(intent);
     }
 }
