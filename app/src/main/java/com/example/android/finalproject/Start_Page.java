2ca1b148ac9c1fe57f9bd1ab3a9d9e9fbd1379cf
A86RD79217 <alex354M@gmail.com>
2018-12-03 23:51:27 -0600

    drinks and sizes complete

--- app/src/main/java/com/example/android/finalproject/Start_Page.java
+++ app/src/main/java/com/example/android/finalproject/Start_Page.java
@@ -1,14 +1,15 @@
 package com.example.android.finalproject;
+
 import android.content.Intent;
 import android.view.View;
-//import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 //import android.support.v7.app.ActionBar;
 //import android.view.Menu;
 //import android.view.MenuItem;
-import android.widget.Toast;
+//import android.widget.TextView;
 
 public class Start_Page extends AppCompatActivity {
     @Override
--- app/src/main/java/com/example/android/finalproject/drinks_and_sizes.java
+++ app/src/main/java/com/example/android/finalproject/drinks_and_sizes.java
@@ -1,13 +1,32 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
 
 public class drinks_and_sizes extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks_and_sizes);
+        Button ageConfirm = (Button) findViewById(R.id.ageconfirm);
+        ageConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+    private void goToMainDashboard() {
+        Intent intent = new Intent(this, Main_dashboard.class);
+        startActivity(intent);
     }
 }
