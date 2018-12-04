fe2535785913be1ba6338ecd8d7cbe1a4af9fbad
A86RD79217 <alex354M@gmail.com>
2018-12-04 17:51:42 -0600

    confirmed buttons work

--- app/src/main/java/com/example/android/finalproject/Party_beer.java
+++ app/src/main/java/com/example/android/finalproject/Party_beer.java
@@ -1,7 +1,15 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
 
 public class Party_beer extends AppCompatActivity {
 
--- app/src/main/java/com/example/android/finalproject/Party_cocktail.java
+++ app/src/main/java/com/example/android/finalproject/Party_cocktail.java
@@ -1,7 +1,16 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
+
 
 public class Party_cocktail extends AppCompatActivity {
 
