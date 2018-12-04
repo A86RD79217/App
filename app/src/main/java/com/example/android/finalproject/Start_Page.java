ed59a6c4b6252df21214837373a5693461543710
A86RD79217 <alex354M@gmail.com>
2018-12-03 22:58:30 -0600

    replaced start page

---
+++ app/src/main/java/com/example/android/finalproject/Start_Page.java
@@ -0,0 +1,31 @@
+package com.example.android.finalproject;
+import android.content.Intent;
+import android.view.View;
+//import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Start_Page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start__page);
+        Button startPage = (Button) findViewById(R.id.startpage);
+        startPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+    private void goToDrinksAndSizes() {
+        Intent intent = new Intent(this, drinks_and_sizes.class);
+        startActivity(intent);
+    }
+}
