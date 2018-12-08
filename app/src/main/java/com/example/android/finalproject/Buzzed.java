454790dac1e157db93a1c7b55a4aa27031c5b6a4
A86RD79217 <alex354M@gmail.com>
2018-12-08 16:09:22 -0600

    finished buzzed category

--- app/src/main/java/com/example/android/finalproject/Buzzed.java
+++ app/src/main/java/com/example/android/finalproject/Buzzed.java
@@ -1,7 +1,15 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
 
 public class Buzzed extends AppCompatActivity {
 
@@ -9,5 +17,47 @@ public class Buzzed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buzzed);
+
+        Button BZbeer = (Button) findViewById(R.id.BZbeer);
+        BZbeer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+                goToBZbeer();
+            }
+        });
+
+        Button BZcocktail = (Button) findViewById(R.id.BZcocktail);
+        BZcocktail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+                goToBZcocktail();
+            }
+        });
+
+        Button BZspirits = (Button) findViewById(R.id.BZspirits);
+        BZspirits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+                goToBZspirits();
+            }
+        });
+    }
+    private void goToBZbeer() {
+        Intent intent = new Intent(this, Buzzed_beer.class);
+        startActivity(intent);
+    }
+    private void goToBZcocktail() {
+        Intent intent = new Intent(this, Buzzed_cocktail.class);
+        startActivity(intent);
+    }
+    private void goToBZspirits() {
+        Intent intent = new Intent(this, Buzzed_spirit.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/finalproject/Buzzed_spirit.java
+++ app/src/main/java/com/example/android/finalproject/Buzzed_spirit.java
@@ -1,7 +1,15 @@
 package com.example.android.finalproject;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.support.v7.app.ActionBar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.TextView;
 
 public class Buzzed_spirit extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class Buzzed_spirit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buzzed_spirit);
+
+        Button goHome = (Button) findViewById(R.id.buzzedspirits);
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It just works!", Toast.LENGTH_SHORT)
+                        .show();
+                gotomaindashboard();
+            }
+        });
+    }
+    private void gotomaindashboard() {
+        Intent intent = new Intent(this, Main_dashboard.class);
+        startActivity(intent);
     }
 }
