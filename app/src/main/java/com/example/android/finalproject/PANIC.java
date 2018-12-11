c5ae9f0319fab12c9989c57c47e6a495716cd6ff
A86RD79217 <alex354M@gmail.com>
2018-12-11 14:19:43 -0600

    calling works

--- app/src/main/java/com/example/android/finalproject/PANIC.java
+++ app/src/main/java/com/example/android/finalproject/PANIC.java
@@ -22,6 +22,8 @@ import android.widget.Button;
 
 public class PANIC extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,15 +31,24 @@ public class PANIC extends AppCompatActivity {
 
         Button panic = (Button) findViewById(R.id.DIAL911);
         panic.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
+            public void onClick(View v) {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:6303865091")); //replace with 911 later
-                if (ActivityCompat.checkSelfPermission(PANIC.this,
-                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    return;
+                callIntent.setData(Uri.parse("tel:16303865091")); //replace with 911 later
+                if (callIntent.resolveActivity(getPackageManager()) != null) {
+                    checkForPhonePermission();
+                    startActivity(callIntent);
                 }
-                startActivity(callIntent);
             }
         });
+    }
+    private void checkForPhonePermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            // Permission not yet granted. Use requestPermissions().
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CALL_PHONE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE);
+        }
     }
 }
